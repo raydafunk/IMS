7a103f242e1b135bce457f1bc0eea6ff93614167
Ray Brown-Amory <r.brown-amory@sky.com>
2023-02-05 16:32:58 +0000

    added inventories list

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -11,17 +11,17 @@ namespace IMS.Plugins.InMemory
         {
             _inventories = new List<Inventory>()
             {
-                new Inventory { InventoryId = 1, InvetoryName = "Bike Seat", Quantity = 10, Price =2},
-                new Inventory { InventoryId = 2, InvetoryName = "Bike Body", Quantity = 10, Price =15},
-                new Inventory { InventoryId = 3, InvetoryName = "Bike Wheels", Quantity = 20, Price =8},
-                new Inventory { InventoryId = 4, InvetoryName = "Bike Pedal", Quantity = 20, Price =1}
+                new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price =2},
+                new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price =15},
+                new Inventory { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price =8},
+                new Inventory { InventoryId = 4, InventoryName = "Bike Pedal", Quantity = 20, Price =1}
             };
         }
         public async Task<IEnumerable<Inventory>> GetInvetoriesByNameAsync(string name)
         {
             if (string.IsNullOrEmpty(name)) return await Task.FromResult(_inventories);
             
-            return _inventories.Where( x => x.InvetoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+            return _inventories.Where( x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
