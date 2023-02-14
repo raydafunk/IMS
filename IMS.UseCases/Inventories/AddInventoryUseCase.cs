62dd5c69b9c592e072fc7692d68246585b90740c
Ray Brown-Amory <r.brown-amory@sky.com>
2023-02-14 21:36:08 +0000

    added  the add inventoires to the project

---
+++ IMS.UseCases/Inventories/AddInventoryUseCase.cs
@@ -0,0 +1,17 @@
+﻿using IMS.CoreBussiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public  class AddInventoryUseCase
+    {
+        public AddInventoryUseCase(IInVentoryRespository inVentoryRespository)
+        {
+
+        }
+        public async Task ExecuteAsync(Inventory inventory)
+        {
+
+        }
+    }
+}
--- IMS.UseCases/PluginInterfaces/IInVentoryRespository.cs
+++ IMS.UseCases/PluginInterfaces/IInVentoryRespository.cs
@@ -4,6 +4,7 @@ namespace IMS.UseCases.PluginInterfaces
 {
     public interface IInVentoryRespository
     {
+        Task AddInventoryAsync(Inventory inventory);
         Task<IEnumerable<Inventory>> GetInvetoriesByNameAsync(string name);
         
     }
