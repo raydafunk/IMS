ca6e3f2f428364e0e7f6d0622fa03d518e180e44
Ray <r.brown-amory@sky.com>
2023-02-14 20:57:05 +0000

    Revert "Feature create inventories"

--- IMS.UseCases/Inventories/Interfaces/IViewInventoriesByNameUserCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS.CoreBussiness;
-
-namespace IMS.UseCases.Inventories.Interfaces
-{
-    public interface IViewInventoriesByNameUserCase
-    {
-        Task<IEnumerable<Inventory>> ExecuteAsync(string name = "");
-    }
-}
