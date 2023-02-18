486e50d1b53066b01fcf01590de3d799a5598544
Ray Brown-Amory <r.brown-amory@sky.com>
2023-02-18 22:46:02 +0000

    added the save and the cancel function to add inventory screen

--- IMS.CoreBussiness/Inventory.cs
+++ IMS.CoreBussiness/Inventory.cs
@@ -1,10 +1,17 @@
-﻿namespace IMS.CoreBussiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBussiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+        [Required]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantitiy must be greater then or equal to 0")]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price  must be greater then or equal to 0")]
         public double Price { get; set; }
     }
 }
