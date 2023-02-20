70758ec4c332f368bab2116a51ae8d666794e6d4
Ray Brown-Amory <r.brown-amory@sky.com>
2023-02-20 00:20:24 +0000

    Added unit test to the case study for automation

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -12,7 +12,7 @@ namespace IMS.Plugins.InMemory
         {
             _inventories = new List<Inventory>()
             {
-                new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price =2},
+                new Inventory { InventoryId = 1, InventoryName = "fdc   ", Quantity = 10, Price =2},
                 new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price =15},
                 new Inventory { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price =8},
                 new Inventory { InventoryId = 4, InventoryName = "Bike Pedal", Quantity = 20, Price =1}
