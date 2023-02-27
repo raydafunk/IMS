37c1d98a4f2048475f29974345c99e2f32332488
Ray Brown-Amory <r.brown-amory@sky.com>
2023-02-27 23:27:12 +0000

    Created the editview component and also added unit testing for the edit view abd tge view Inventory Untit tests

---
+++ IMS.UseCase.Test/Inventories/ViewInventoryByIdUserCaseTest.cs
@@ -0,0 +1,48 @@
+﻿using IMS.CoreBussiness;
+using IMS.UseCases.Inventories;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using NSubstitute;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IMS.UseCase.Test.Inventories
+{
+    public class ViewInventoryByIdUserCaseTest
+    {
+        private readonly IInVentoryRespository _inVentoryRespository;
+
+        private readonly ViewInventoryByIdUseCase _viewInventoriesByIduseCase;
+
+        public ViewInventoryByIdUserCaseTest()
+        {
+            _inVentoryRespository = Substitute.For<IInVentoryRespository>();
+            _viewInventoriesByIduseCase = new ViewInventoryByIdUseCase(_inVentoryRespository);
+        }
+
+        [Fact]
+        public async Task ViewInventoryByIdUseCase_WhenCalled_Should_ReturnObject()
+        {
+            var inventoryId = 1;
+
+            await _viewInventoriesByIduseCase.ExeuteAysnc(inventoryId);
+
+            inventoryId.ShouldBe(inventoryId);
+        }
+
+        [Fact]
+        public async Task ViewInventoryByIdUseCase_WhenCalled_ShouldHaveReturnID()
+        {
+            var inventoryId = 1;
+
+            await _viewInventoriesByIduseCase.ExeuteAysnc(inventoryId);
+
+            inventoryId.ShouldBeSameAs(inventoryId);
+        }
+    }
+}
