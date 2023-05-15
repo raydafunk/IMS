069f852dca19e04fc67ce489a35b59a1805f7d7e
Ray Brown-Amory <r.brown-amory@sky.com>
2023-05-15 22:58:46 +0100

    added  the Produce component to the system

---
+++ IMS.WebApp/ViewModels/ProducVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class ProductVM
+    {
+        [Required]
+        public string? ProductNumber { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "You must to select a product")]
+        public int ProductId { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quanity has to be greater then 1")]
+
+        public int QuantityToProduct { get; set; }
+    }
+}
